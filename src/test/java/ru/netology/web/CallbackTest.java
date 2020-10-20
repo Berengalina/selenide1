b917cffa0f3bab3098c5920851238bd84801327d
Anna Belousova <annabelousovva@gmail.com>
2020-10-20 21:26:42 +0300

    Add

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,22 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CallbackTest {
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:7777");
+        SelenideElement form = $(byClassName("form_theme_alfa-on-white"));
+        form.$("[data-test-id=name] input").setValue("Белоусова Анна");
+        form.$("[data-test-id=phone] input").setValue("+79266858100");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=submit]").click();
+        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+    }
+}
