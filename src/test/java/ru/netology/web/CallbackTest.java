272d96614afc955b3994a4a5c29ea2f63405d562
Anna Belousova <annabelousovva@gmail.com>
2020-10-22 19:14:41 +0300

    Imorive CallbackTest.java

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -27,7 +27,7 @@ class CallbackTest {
         form.$("[data-test-id=phone] input").setValue("+79266858100");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").shouldHave(exactText("Фамилия и имя Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid").shouldHave(exactText("Фамилия и имя Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
 
@@ -39,7 +39,7 @@ class CallbackTest {
         form.$("[data-test-id=phone] input").setValue("79266858100");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").shouldHave(exactText("Мобильный телефон Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid").shouldHave(exactText("Мобильный телефон Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
 
@@ -50,7 +50,7 @@ class CallbackTest {
         form.$("[data-test-id=name] input").setValue("Белоусова Анна");
         form.$("[data-test-id=phone] input").setValue("+79266858100");
         form.$("button").click();
-        $(".input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $("[data-test-id=agreement].input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
     @Test
@@ -61,7 +61,7 @@ class CallbackTest {
         form.$("[data-test-id=phone] input").setValue("+79266858100");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").shouldHave(exactText("Фамилия и имя Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid").shouldHave(exactText("Фамилия и имя Поле обязательно для заполнения"));
     }
 
     @Test
@@ -72,7 +72,7 @@ class CallbackTest {
         form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".input_invalid").shouldHave(exactText("Мобильный телефон Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid").shouldHave(exactText("Мобильный телефон Поле обязательно для заполнения"));
     }
 
 }
